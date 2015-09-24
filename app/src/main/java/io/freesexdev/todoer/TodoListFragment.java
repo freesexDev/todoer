8ddaf18400fa233f63b817297e7b743f6f39cf3a
FreesexDev <spoodymom@gmail.com>
2015-09-24 22:10:01 +0300

    Fixed drawer header

--- app/src/main/java/io/freesexdev/todoer/TodoListFragment.java
+++ app/src/main/java/io/freesexdev/todoer/TodoListFragment.java
@@ -1,6 +1,6 @@
 package io.freesexdev.todoer;
 
-import android.app.ListFragment;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class TodoListFragment extends ListFragment {
+public class TodoListFragment extends Fragment {
 
     private LayoutInflater inflater;
 
